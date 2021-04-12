68af69bb959c148c8d359c00f7b1b06a2e4b82fa
Crenshaw <lazerspam101@gmail.com>
2021-04-11 17:34:58 -0700

    Create flagParse.js

---
+++ flagParse.js
@@ -0,0 +1,54 @@
+exports.flagParse = async (flags, string) => {
+    // find all groups
+    let located = string.match(/"[^"]*"|[^"\s]*/img).filter(str => str !== '');
+    const map = new Map();
+    let currentFlag = "";
+    // combine two that are next to eachother
+    for (let current of located) {
+        // make and set currentFlag
+        if (current.match(/^-/ig)) {
+           for (let flag of flags) {
+               if (flag.name == current.slice(1)) {
+                   map.set(flag.name, []);
+                   currentFlag = current.slice(1);
+               }
+           }
+           continue;
+        }
+
+        // remove the " if it's there
+        current = current.replace(/^\"|\"$/g, "");
+
+        // add to an exsisting flag (ignoring noFlag)
+        if (currentFlag && currentFlag !== "noFlag") {
+            let lastMapIndex = map.get(currentFlag).length;
+            let flagArgs = "";
+            for (let flag of flags) {
+                if (flag.name == currentFlag) flagArgs = flag.args;
+            }
+    
+            // change the type
+            if (current.match(/^(t(rue)?|f(alse)?|1|0)$/i)) {
+                current = Boolean(current);
+            } else if (current.match(/^\d+$/)) {
+                current = Number(current);
+            }
+    
+            // if valid type for arg, add to map
+            if (typeof current === flagArgs[lastMapIndex]) {
+                map.get(currentFlag).push(current)
+            }
+
+        // There was no flag defiend to add to
+        } else {
+            if (!map.get("noFlag")) {
+                map.set("noFlag", [current]);
+                currentFlag = "noFlag"
+                continue
+            }
+            map.get("noFlag").push(current);
+        }
+    }
+
+    return map;
+}
