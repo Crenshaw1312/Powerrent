f8f8c1cb36ca474470dd395165e4704ece552137
Crenshaw <lazerspam101@gmail.com>
2021-04-13 22:58:56 -0700

    Added "pres" flag
    you can now search for the currently playing song, given that you have a status that's "Listening to..."

--- commands/search.js
+++ commands/search.js
@@ -1,5 +1,5 @@
 const { open: openModal } = require('powercord/modal');
-const { React } = require('powercord/webpack');
+const { React, getModule } = require('powercord/webpack');
 const SearchResults = require('../components/SearchResults');
 const TorrentSearchApi = require('torrent-search-api');
 const { flagParse } = require("../flagParse");
@@ -9,14 +9,37 @@ module.exports = {
     description: "Search for torrents",
     executor: async (args, main) => {
 
+        // setup flags
+        let flags = [
+            {name: "site", args: ["string"]},
+            {name: "cate", args: ["string"]},
+            {name: "max", args: ["number"]},
+            {name: "pres", args: ["booleen"]}
+        ]
+        let parameters = await flagParse(flags, args.join(" "));
+
         // need args
-        if (!args[0]) {
-            return powercord.api.notices.sendToast('Powerrent', {
+        if (!parameters.get("noFlag") && !parameters.get("pres")) {
+            return powercord.api.notices.sendToast('PowerrentNotif', {
                 header: `Please provide a search`,
                 timeout: 3e3
             })
         }
-        // enable providers
+
+        // presence flag / setting up search
+        let search;
+        let category = 'All';
+        if (parameters.get('pres')) {
+            let activityStore = await getModule(["getActivities"],)
+            let activity = activityStore.getActivities(getModule(['getCurrentUser'], false).getCurrentUser().id)
+            if (activity = activity.find(act => act.type == 2)) {
+                search = activity.details
+                category = "Music"
+            }
+        } else {
+            search = parameters.get("noFlag").join(" ")
+        }
+
         // enable any PTs that there's a username and password for
         TorrentSearchApi.getProviders().filter(p => !p.public).map((provider) => {
             let username = main.settings.get(`${provider.name}Username`)
@@ -27,36 +50,28 @@ module.exports = {
                 TorrentSearchApi.enableProvider(provider.name, username, password)
             } catch (error) {
                 // failed
-                powercord.api.notices.sendToast('Powerrent', {
+                powercord.api.notices.sendToast('PowerrentNotif', {
                     header: `Failed to log into ${provider.name}`,
                     content: `(Username: ${username}) (Password: ${password})`,
                     timeout: 10e3
                 })
-                console.error(error)
             }
         })
-       TorrentSearchApi.enablePublicProviders()
-       for (let provider of (main.settings.get('blacklistedSites') || ["ThePirateBay", "KickassTorrents", "Yts"])) {
+        // enable providers
+        TorrentSearchApi.enablePublicProviders()
+        for (let provider of (main.settings.get('blacklistedSites') || ["ThePirateBay", "KickassTorrents", "Yts"])) {
            TorrentSearchApi.disableProvider(provider)
-       }
+        }
 
-        // setup flags
-        let flags = [
-            {name: "site", args: ["string"]},
-            {name: "cate", args: ["string"]},
-            {name: "max", args: ["number"]}
-        ]
-        let parameters = await flagParse(flags, args.join(" "));
-
-        // deal with site
+        // deal with site flag
         let tracker = parameters.get('site')
         if (tracker) {
             try {
-            tracker = TorrentSearchApi.getProvider(tracker[0])
+                tracker = TorrentSearchApi.getProvider(tracker[0])
                 TorrentSearchApi.disableAllProviders()
                 TorrentSearchApi.enableProvider(tracker.name)
             } catch (err) {
-                return powercord.api.notices.sendToast('Powerrent', {
+                return powercord.api.notices.sendToast('PowerrentNotif', {
                     header: `Invalid site/tracker name`,
                     timeout: 3e3
                 })
@@ -65,7 +80,6 @@ module.exports = {
         }
 
         // deal with category
-        let category = 'All'
         let cate = parameters.get("cate")
         let noSearch = [];
         if (cate) {
@@ -81,7 +95,7 @@ module.exports = {
 
             // show notif for not-search trackers
             if (noSearch[0]) {
-                await powercord.api.notices.sendToast('Powerrent', {
+                powercord.api.notices.sendToast('PowerrentNotif', {
                     header: `Could not search all trackers for the ${category.toLowerCase()} category`,
                     content: noSearch.join(", "),
                     timeout: 10e3
@@ -90,15 +104,15 @@ module.exports = {
         }
 
         // search
-        const torrents = await TorrentSearchApi.search(parameters.get("noFlag").join(" "), category, parameters.get('max') ? parameters.get('max')[0] : main.settings.get('searchMax'));
+        const torrents = await TorrentSearchApi.search(search, category, parameters.get('max') ? parameters.get('max')[0] : main.settings.get('searchMax'));
         // no results
         if (!torrents[0]) {
-            return powercord.api.notices.sendToast('Powerrent', {
-                header: `No results`,
+            return powercord.api.notices.sendToast('PowerrentNotif', {
+                header: `No results for \"${search}\" in ${category}`,
                 timeout: 3e3
             })
         }
-        return openModal(() => React.createElement(SearchResults, {results: torrents, search: `Searched for \"${parameters.get("noFlag").join(" ")}\" in ${category} - \\  _ \\`}));
+        return openModal(() => React.createElement(SearchResults, {results: torrents, search: `Searched for \"${search}\" in ${category} - \\  _ \\`}));
 
     }
 }
